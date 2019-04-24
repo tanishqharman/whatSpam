e94800d46cb06ea5679eb2267cc19c3897d91f7b
Tanishq Harman <30750568+tanishqharman@users.noreply.github.com>
2019-04-24 22:07:58 +0530

    Update recent_chat_contacts.py

--- recent_chat_contacts.py
+++ recent_chat_contacts.py
@@ -6,18 +6,15 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-# Replace below path with the absolute path
-# to chromedriver in your computer
+# path of chrome-webdriver in your computer
 driver = webdriver.Chrome('/home/tan_ishq/Desktop/whatSpam/chromedriver')
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
-
 
 group_title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "div.RLfQR")))
 
 time.sleep(5)
 for person in driver.find_elements_by_class_name('_2wP_Y'):
     title = person.find_element_by_xpath('div/div/div[2]/div[1]/div[1]/span').text
-    #company = person.find_element_by_xpath('.//div[@class="company"]/a').text
     print(title)
