926f747676a97042e242a6be849dba89e9149911
Tanishq Harman <30750568+tanishqharman@users.noreply.github.com>
2019-04-24 22:02:51 +0530

    second commit

--- send_msg.py
+++ send_msg.py
@@ -6,18 +6,16 @@ from selenium.webdriver.common.by import By
 import time
 import sys
 
-# Replace below path with the absolute path
-# to chromedriver in your computer
-driver = webdriver.Chrome('/home/tan_ishq/Desktop/whatSpam/chromedriver')
+# path where chrome-webdriver is located
+driver = webdriver.Chrome('/home/tan_ishq/Desktop/whatSpam/chromedriver') 
 
 driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600)
 
-# Replace 'Friend's Name' with the name of your friend
-# or the name of a group
+# contact name or group name whom you want to send message
 target = '"W Navneet Rawat"'
 
-# Replace the below string with your own message
+# write the message
 string = "Unblock kar....BSDK!!"
 
 x_arg = '//span[contains(@title,' + target + ')]'
@@ -25,9 +23,9 @@ group_title = wait.until(EC.presence_of_element_located((
 	By.XPATH, x_arg)))
 group_title.click()
 
-
 message = driver.find_elements_by_xpath('//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')[0]
 
+# set the frequency of message in range
 for i in range(500):
 	message.send_keys(string + Keys.ENTER)
 
